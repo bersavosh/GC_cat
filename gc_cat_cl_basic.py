cb922802508a18d3329354507c564e57d7c85ffe
Arash Bahramian <bahramia@ualberta.ca>
2017-06-04 18:39:48 -0400

    catalog maker run

--- gc_cat_cl_basic.py
+++ gc_cat_cl_basic.py
@@ -1,13 +1,15 @@
 import sys, os, inspect, urllib2
 
-"""
 online = True
 
 if online == True:
     try:
-		gc_cat = urllib2.urlopen('https://github.com/bersavosh/GC_cat/raw/master/gc_cat.txt').readlines()
+        request = urllib2.Request('https://github.com/bersavosh/GC_cat/raw/master/gc_cat.txt')
+        request.add_header('Pragma', 'no-cache')
+        content = urllib2.build_opener().open(request)
+        gc_cat = urllib2.urlopen(request).readlines()
     except:
-    	print 'offfline'
+    	print 'offline'
         cat_file = os.path.abspath(inspect.getfile(inspect.currentframe())).replace('gc_cat_cl_basic.py','gc_cat.txt')
         with open(cat_file) as f:
             gc_cat = f.readlines()
@@ -16,13 +18,6 @@ if online == False:
 	cat_file = os.path.abspath(inspect.getfile(inspect.currentframe())).replace('gc_cat_cl_basic.py','gc_cat.txt')
 	with open(cat_file) as f:
 		gc_cat = f.readlines()
-"""
-
-request = urllib2.Request('https://github.com/bersavosh/GC_cat/raw/master/gc_cat.txt')
-request.add_header('Pragma', 'no-cache')
-
-gc_cat = urllib2.urlopen('https://github.com/bersavosh/GC_cat/raw/master/gc_cat.txt').readlines()
-
 
 if len(sys.argv) == 1:
     query = raw_input('Enter gc name: ')
